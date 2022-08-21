6942f5712ac82fe8fafd881218bfdcd4f081dde0
Andrea <bbooss97@gmail.com>
2022-08-21 11:51:47 +0200

    refactored the code and added mlp  made ppi work

--- nn.py
+++ nn.py
@@ -2,38 +2,74 @@ import torch
 from torch_geometric.nn import GCNConv, GATv2Conv
 import torch.nn.functional as F
 
-
-
+#this is the graph attention network module
 class GraphAttentionNetwork(torch.nn.Module):
   """Graph Attention Network"""
   def __init__(self, dim_in, dim_h, dim_out, heads=8):
     super().__init__()
+    #we have 2 graph attention layers specifying dimension of inputs,outputs and heads
     self.gat1 = GATv2Conv(dim_in, dim_h, heads=heads)
     self.gat2 = GATv2Conv(dim_h*heads, dim_out, heads=1)
-
-
   def forward(self, data):
+    #i get the input features and the graph connections as input x
     x, edge_index=data.x,data.edge_index
+    #i filter the input features with a dropout layer avoid overfitting and improve generalization
     h = F.dropout(x, p=0.6, training=self.training)
+    #i apply the first graph attention layer
     h = self.gat1(x, edge_index)
+    #i apply a elu activation function as in the paper
     h = F.elu(h)
+    #i filter the features h with a dropout layer avoid overfitting and improve generalization
     h = F.dropout(h, p=0.6, training=self.training)
+    #i apply the second graph attention layer
     h = self.gat2(h, edge_index)
-    return F.log_softmax(h, dim=1)   
+    #i return the logsoftmax of the output to normalize the output
+    return h
 
+#this is the graph convolutional network module
 class GraphConvolutionalNetwork(torch.nn.Module):
-
     def __init__(self, in_size, h_size,out_size):
         super().__init__()
+        #we have 2 graph convolutional layers specifying dimension of inputs,outputs and hiddend features
         self.conv1 = GCNConv(in_size, h_size)
         self.conv2 = GCNConv(h_size, out_size)
 
     def forward(self, data):
+        #i get the input features and the graph connections as input x
         x, edge_index = data.x, data.edge_index
-
+        #dropout layer to avoid overfitting and improve generalization
+        x = F.dropout(x, p=0.6, training=self.training)
+        #i apply the first graph convolutional layer
         x = self.conv1(x, edge_index)
+        #i apply a relu activation function 
         x = F.relu(x)
-        x = F.dropout(x, training=self.training)
+         #i filter the features x with a dropout layer avoid overfitting and improve generalization
+        x = F.dropout(x, p=0.6, training=self.training)
+        #i apply the second graph convolutional layer
         x = self.conv2(x, edge_index)
+        #i return the logsoftmax of the output to normalize the output
+        return x
 
-        return F.log_softmax(x, dim=1)
+
+class Mlp(torch.nn.Module):
+    def __init__(self, in_size, h_size,out_size):
+        super().__init__()
+        #we have 2 graph convolutional layers specifying dimension of inputs,outputs and hiddend features
+        self.fc1 = torch.nn.Linear(in_size, h_size)
+        self.fc2 = torch.nn.Linear(h_size, out_size)
+
+    def forward(self, data):
+        #i get the input features and the graph connections as input x
+        x, edge_index = data.x, data.edge_index
+        #dropout layer to avoid overfitting and improve generalization
+        x = F.dropout(x, p=0.6, training=self.training)
+        #i apply the first graph convolutional layer
+        x = self.fc1(x)
+        #i apply a relu activation function 
+        x = F.relu(x)
+         #i filter the features x with a dropout layer avoid overfitting and improve generalization
+        x = F.dropout(x, p=0.6, training=self.training)
+        #i apply the second graph convolutional layer
+        x = self.fc2(x)
+        #i return the logsoftmax of the output to normalize the output
+        return x
--- train.py
+++ train.py
@@ -1,89 +1,128 @@
 from torch_geometric.datasets import PPI
 import torch
 from torch_geometric.loader import DataLoader
-from nn import GraphAttentionNetwork, GraphConvolutionalNetwork
+from nn import GraphAttentionNetwork, GraphConvolutionalNetwork, Mlp
 import wandb
 from torch_geometric.datasets import Planetoid
 import torch.nn.functional as F
 
+
 class Trainer:
-    def __init__(self,model,optimizer,batchsize,epochs,lossfn,percentage,datasetName,type="mlp"):
+    def __init__(self,model,optimizer,epochs,datasetName):
         self.model=model
-        self.percentage=percentage
-        self.type=type
         self.optimizer=optimizer
-        self.batchsize=batchsize
         self.epochs=epochs
-        self.lossfn=lossfn
         self.loadDataset(datasetName)
 
+    #training loop
     def train(self):
         it=0
         self.model.train()
         for epoch in range(self.epochs):
             it+=1    
-            self.optimizer.zero_grad()        
+            self.optimizer.zero_grad()    
+            # if the database is ppi then the task is the multilabeling classification 
+            #otherwise is node classification
             if self.datasetName=="ppi":
                 out = model(self.dataset[0])
+                #the loss in this case is the binary cross entropy with logits loss
+                #the logits are numbers between -inf and inf not yet normalized for every class
+                #in this case i have multiple graphs so i just pass the first graph to train on
+                #i could improve it to train on all the others like in a kfold cross validation
                 loss=torch.nn.BCEWithLogitsLoss(reduction='mean')(out, self.dataset[0].y)
             else:
                 out = model(self.dataset)
+                #the loss here is the negative log likelihood 
+                #i mask the graph since i have just one graph to train and then i evaluate with a different mask
+                #i softmax the output to apply the nll loss
+                out=F.log_softmax(out, dim=1)
                 loss = F.nll_loss(out[self.dataset.train_mask], self.dataset.y[self.dataset.train_mask])
+            #backpropagation
             loss.backward()
             self.optimizer.step()
+            print("mean_loss_train",loss.data.mean())
+            wandb.log({"mean_loss_train":loss.data.mean()})
+            #i evaluate the model on the test part of the graph
             self.test()
-            #         print("Epoch:",epoch,"Iteration:",it,"Loss:",loss.data.mean(),"Training accuracy:",accuracy)
-            #         wandb.log({"epoch_train":epoch,"iteration_train":it,"loss_train":loss.data.mean(),"accuracy_train":accuracy})
+
     def test(self):
         print("testing phase")
         model.eval()
-        if self.datasetName=="ppi":
-            pred = model(self.dataset[1])
-            y=self.dataset[1].y
-            acc=torch.nn.BCEWithLogitsLoss(reduction='mean')(pred, y)
-            # correct = (pred == y).sum()
-            # # acc=F1Score(121,0)(pred,y)
-            # acc = int(correct) / int(self.dataset[1].y.shape[0])
-        else:
-            pred = model(self.dataset).argmax(dim=1)
-            correct = (pred[self.dataset.test_mask] == self.dataset.y[self.dataset.test_mask]).sum()
-            acc = int(correct) / int(self.dataset.test_mask.sum())
-        print(f'Accuracy: {acc:.4f}')
+        #if the dataset is ppi then the task is the multilabeling classification
+        #so i get the output and then i apply the sigmoid to get the probability of each class
+        #i threshold the probability to get the class for each label
+        #then i compare the output with the ground truth
+        with torch.no_grad():
+            if self.datasetName=="ppi":
+                #i evaluate the model on the second graph of the dataset not the first of the trainign
+                pred = model(self.dataset[1])
+                y=self.dataset[1].y
+                loss=torch.nn.BCEWithLogitsLoss(reduction='mean')(pred, y)
+                pred=F.sigmoid(pred)
+                pred=torch.where(pred>0.5,1,0)
+                correct=(pred==y).sum()
+                acc=correct/len(y)
+                # correct = (pred == y).sum()
+                # # acc=F1Score(121,0)(pred,y)
+                # acc = int(correct) / int(self.dataset[1].y.shape[0])
+            else:
+                #if the dataset is cora, citeseer or pubmed then the task is the node classification
+                #so i have the nll loss at the end on the log softmax output
+                pred = model(self.dataset)
+                #i softmax the prdictions and i apply the negative log likelihood to the test graph (masked)and the argmax to get the class
+                pred=F.log_softmax(pred, dim=1)
+                loss = F.nll_loss(pred[self.dataset.test_mask], self.dataset.y[self.dataset.test_mask])
+                #calculate accuracy
+                pred=pred.argmax(dim=1)
+                correct = (pred[self.dataset.test_mask] == self.dataset.y[self.dataset.test_mask]).sum()
+                acc = int(correct) / int(self.dataset.test_mask.sum())
 
-        # print("mean_loss_test",test_loss/it,"mean_accuracy_test:",test_accuracy/it)
-        # wandb.log({"mean_loss_test":test_loss/it,"mean_accuracy_test":test_accuracy/it})
+        #log the results
+        print("mean_loss_test",loss.data.mean(),"mean_accuracy_test:",acc)
+        wandb.log({"mean_loss_test":loss.data.mean(),"mean_accuracy_test":acc})
 
-    def loadDataset(self,datasetName="cora",type="train"):
+
+    #this function loads the dataset based on the name 
+    def loadDataset(self,datasetName="cora"):
         self.datasetName = datasetName
         #should be cora, citeseer and pubmed or ppi
         if datasetName=="ppi":
             self.dataset=PPI(root="./data/ppi")
         else:
             self.dataset=Planetoid(name=datasetName,root="./data/"+datasetName)
+        #print the dataset generalities
         print("name of dataset: {},number of classes: {},number of nodes features: {},number of graphs: {}".format(self.dataset,self.dataset.num_classes,self.dataset.num_node_features,len(self.dataset)))
         if datasetName!="ppi":
             self.dataset=self.dataset[0]
-        
-inOut={
-    "cora":(1433,7),
-    "citeseer":(3703,6),
-    "pubmed":(500,3),
-    "ppi":(50,121)
-}
-datasetName="ppi"
-type="gat"
-device = torch.device('cpu')
-if type=="gcn":
-    model=GraphConvolutionalNetwork(inOut[datasetName][0],13,inOut[datasetName][1])
-elif type=="gat":
-    model=GraphAttentionNetwork(inOut[datasetName][0],13,inOut[datasetName][1])
 
-model.to(device)
-# wandb.watch(model)
-batchsize=5
-epochs=1000
-loss=torch.nn.CrossEntropyLoss()
 
-optmimizer=torch.optim.Adam(model.parameters())
-trainer=Trainer(model,optmimizer,batchsize,epochs,loss,type=type,percentage=1,datasetName=datasetName)
-trainer.train()
+if __name__ == "__main__":
+    # this is the number of features of the input and the output dimension that the model will have based on the dataset
+    inOut={
+        "cora":(1433,7),
+        "citeseer":(3703,6),
+        "pubmed":(500,3),
+        "ppi":(50,121)
+    }
+    # i set the name of the dataset here and the type of model
+    datasetName="pubmed"
+    type="gcn"
+    device = torch.device('cpu')
+    #i create the model based on the settings of dataset name and type
+    if type=="gcn":
+        model=GraphConvolutionalNetwork(inOut[datasetName][0],13,inOut[datasetName][1])
+    elif type=="gat":
+        model=GraphAttentionNetwork(inOut[datasetName][0],13,inOut[datasetName][1])
+    elif type=="mlp":
+        model=Mlp(inOut[datasetName][0],50,inOut[datasetName][1])
+    # i create the the wandb project and the run    
+    name=type+" "+datasetName
+    run=wandb.init(project='graphAttentionNetwork', entity='bbooss97',name=name)
+    model.to(device)
+    run.watch(model)
+    #define the settings for the training
+    epochs=200
+    optmimizer=torch.optim.Adam(model.parameters())
+    #create the trainer and start the training process
+    trainer=Trainer(model,optmimizer,epochs,datasetName=datasetName)
+    trainer.train()
